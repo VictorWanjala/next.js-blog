85024bd6a327b9af6bc372e7e56b992bf38a524a
Victor <wanjala94vic@gmail.com>
2024-04-09 09:36:14 +0300

    updating blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 import { client, urlFor } from "./lib/sanity";
 import { simpleBlogCard } from "./lib/interface";
-import Link from "next/link";
 
 async function getData() {
   const query = `*[_type == 'blog']{
@@ -18,25 +18,32 @@ async function getData() {
 
 export default async function Home() {
   const data: simpleBlogCard[] = await getData();
-  console.log(data);
 
   return (
     <div className="flex justify-center">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-6xl">
-        {/* Title and small text */}
-        <div className="mb-2 col-span-full">
-          <h1 className="text-3xl font-bold">The Accessibility Blog</h1>
-          <p className="text-sm text-gray-500">The voice of the excluded</p>
+        <div className="col-span-full">
+          <h1 className="text-3xl font-bold mb-2">The Accessibility Blog</h1>
+          <p className="text-sm text-gray-500 mb-4">The voice of the excluded</p>
         </div>
 
         {data.map((post, idx) => (
           <div key={idx} className="p-4">
             <Link href={`/blog/${post.currentSlug}`}>
-              <div className="flex flex-col items-center">
-                <Image src={urlFor(post.titleImage).url()} alt="image" width={500} height={500} className="rounded-lg" />
-                <p className="font-bold mt-2">{post.publicationDate}</p>
-                <h2 className="text-xl font-bold mt-2 line-clamp-2">{post.title}</h2>
-                <p className="line-clamp-3 text-sm mt-2 text-gray-600">{post.smallDescription}</p>
+              <div className="block rounded-lg overflow-hidden hover:shadow-lg">
+                <div className="relative h-60">
+                  <Image
+                    src={urlFor(post.titleImage).url()}
+                    alt="image"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+                <div className="p-4">
+                  <p className="font-bold">{post.publicationDate}</p>
+                  <h2 className="text-xl font-bold mt-2 mb-4">{post.title}</h2>
+                  <p className="line-clamp-3 text-sm text-gray-600">{post.smallDescription}</p>
+                </div>
               </div>
             </Link>
           </div>
@@ -45,6 +52,7 @@ export default async function Home() {
     </div>
   );
 }
+
 
 
 
